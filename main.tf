0209d06e467e63da3c0ab3a6cd8174b199aed61a
ch1aki <sugawara256@gmail.com>
2019-08-18 15:17:30 +0900

    fmt

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ data "aws_iam_policy_document" "allow_describe_regions" {
 }
 
 module "describe_regions_for_ec2" {
-    source = "./iam_role"
-    name = "describe-regions-for-ec2"
-    identifier = "ec2.amazonaws.com"
-    policy = data.aws_iam_policy_document.allow_describe_regions.json
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
